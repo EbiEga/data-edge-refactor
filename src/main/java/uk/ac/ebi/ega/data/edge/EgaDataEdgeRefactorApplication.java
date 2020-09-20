ae949820ce981e28d3214ba13f550bbebd424856
jorizci <jorizci@gmail.com>
2020-09-20 17:11:02 +0100

    Added base spring-boot project

---
+++ src/main/java/uk/ac/ebi/ega/data/edge/EgaDataEdgeRefactorApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.ega.data.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgaDataEdgeRefactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgaDataEdgeRefactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/ebi/ega/data/edge/EgaDataEdgeRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.ega.data.edge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaDataEdgeRefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
