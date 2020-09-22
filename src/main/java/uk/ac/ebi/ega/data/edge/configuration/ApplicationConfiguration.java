d0096f6be635288a4ca5324d72c8063d3a3f1116
jorizci <jorizci@gmail.com>
2020-09-22 12:31:15 +0100

    First draft of a reactive file streaming service through S3

--- src/main/java/uk/ac/ebi/ega/data/edge/configuration/ApplicationConfiguration.java
+++ src/main/java/uk/ac/ebi/ega/data/edge/configuration/ApplicationConfiguration.java
@@ -17,25 +17,80 @@
  */
 package uk.ac.ebi.ega.data.edge.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3AsyncClientBuilder;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import uk.ac.ebi.ega.data.edge.configuration.properties.S3Properties;
 import uk.ac.ebi.ega.data.edge.service.FileMetaService;
+import uk.ac.ebi.ega.data.edge.service.FileStreamingService;
 import uk.ac.ebi.ega.data.edge.service.KeyService;
 import uk.ac.ebi.ega.data.edge.service.internal.KeyServiceImpl;
 import uk.ac.ebi.ega.data.edge.service.internal.RemoteFileMetaServiceImpl;
+import uk.ac.ebi.ega.data.edge.service.internal.S3FileStreamingService;
+
+import java.net.URI;
+import java.time.Duration;
 
 @Configuration
 public class ApplicationConfiguration {
 
     @Bean
-    public KeyService keyService(WebClient.Builder webClientBuilder){
+    public KeyService keyService(WebClient.Builder webClientBuilder) {
         return new KeyServiceImpl(webClientBuilder.build());
     }
 
     @Bean
-    public FileMetaService fileMetaService(WebClient.Builder webClientBuilder){
+    public FileMetaService fileMetaService(WebClient.Builder webClientBuilder) {
         return new RemoteFileMetaServiceImpl(webClientBuilder.build());
+    }
+
+    @Bean
+    public FileStreamingService fileStreamingService(S3AsyncClient s3AsyncClient, S3Properties s3Properties) {
+        return new S3FileStreamingService(s3AsyncClient, s3Properties);
+    }
+
+    @Bean
+    public S3AsyncClient s3AsyncClient(S3Properties s3Properties) {
+
+        SdkAsyncHttpClient httpClient = NettyNioAsyncHttpClient.builder()
+                .writeTimeout(Duration.ZERO)
+                .maxConcurrency(64)
+                .build();
+
+        S3Configuration serviceConfiguration = S3Configuration.builder()
+                .checksumValidationEnabled(false)
+                .chunkedEncodingEnabled(true)
+                .build();
+
+        S3AsyncClientBuilder b = S3AsyncClient.builder()
+                .httpClient(httpClient)
+                .credentialsProvider(() -> AwsBasicCredentials.create(s3Properties.getAccessKeyId(), s3Properties.getSecretAccessKey()))
+
+                .serviceConfiguration(serviceConfiguration);
+
+        if (s3Properties.getRegion() != null && !s3Properties.getRegion().isEmpty()) {
+            b = b.region(Region.of(s3Properties.getRegion()));
+        }
+
+        if (s3Properties.getEndpoint() != null && !s3Properties.getEndpoint().isEmpty()) {
+            b = b.endpointOverride(URI.create(s3Properties.getEndpoint()));
+        }
+
+        return b.build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "data.edge.s3")
+    public S3Properties s3Properties() {
+        return new S3Properties();
     }
 
 }
